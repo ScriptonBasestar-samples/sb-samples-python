10b12ee6081a1d954069968cf9acacd69b4e195c
cee <archmagece@gmail.com>
2016-12-09 10:54:12 +0900

    샘플추가

--- sample_oracle.py
+++ sample_oracle.py
@@ -10,3 +10,8 @@ row = cursor.fetchone()
 while row:
     print(str(row[0]) + " " + str(row[1]) + " " + str(row[2]) + " " + str(row[3]))
     row = cursor.fetchone()
+
+    cursor.setinputsizes(PRODID=None, SEQ=None, IMAGENAME=None, DETAILDESC=cx_Oracle.CLOB)
+    cursor.execute("""INSERT INTO sso_data.t_users (PRODID, SEQ, IMAGENAME, DETAILDESC)
+ VALUES (:PRODID, :SEQ, :IMAGENAME, :DETAILDESC)""",
+                   PRODID=str(row[0]), SEQ=str(row[1]), IMAGENAME=str(row[2]), DETAILDESC=str(row[4]))
