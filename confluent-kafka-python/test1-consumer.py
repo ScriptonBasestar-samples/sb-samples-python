870da87b98b3b77a2b0520adfa10bdfd67a8e8e1
CEE <archmagece@gmail.com>
2020-08-13 16:25:29 +0900

    confluent kafka sample

---
+++ confluent-kafka-python/test1-consumer.py
@@ -0,0 +1,26 @@
+import os
+
+import dotenv
+from confluent_kafka import Consumer
+
+dotenv.load_dotenv()
+c = Consumer({
+    'bootstrap.servers': os.getenv('KAFKA_HOST', 'localhost:9092'),
+    'group.id': 'mygroup',
+    'auto.offset.reset': 'earliest'
+})
+
+c.subscribe(['mytopic'])
+
+while True:
+    msg = c.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+c.close()
